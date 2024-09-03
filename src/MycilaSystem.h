287085ea4cc96deae56ce35f9996065966d77e41
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-09-03 15:50:38 +0200

    v3.0.0

--- src/MycilaSystem.h
+++ src/MycilaSystem.h
@@ -13,10 +13,10 @@
 #include <ArduinoJson.h>
 #endif
 
-#define MYCILA_SYSTEM_VERSION "2.0.8"
-#define MYCILA_SYSTEM_VERSION_MAJOR 2
+#define MYCILA_SYSTEM_VERSION "3.0.0"
+#define MYCILA_SYSTEM_VERSION_MAJOR 3
 #define MYCILA_SYSTEM_VERSION_MINOR 0
-#define MYCILA_SYSTEM_VERSION_REVISION 8
+#define MYCILA_SYSTEM_VERSION_REVISION 0
 
 namespace Mycila {
   class System {
